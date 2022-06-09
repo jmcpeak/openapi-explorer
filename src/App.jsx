ebde832f1a780a68d961097c5cb57f035675e91d
Jason McPeak <jmcpeak@payrailz.com>
2022-06-09 11:54:43 -0700

    use our example

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import './App.css';
 
 export default function App() {
   return (
-      <openapi-explorer spec-url="http://localhost:3000/test.json" />
+      <openapi-explorer
+        spec-url="http://localhost:3000/test.json"
+        nav-bg-color="#732282" text-color="#555555"
+        secondary-color="#f3e5f5" />
   );
 }
