a11086c0455c1874f17d5905bd8b079be951499a
Jason McPeak <jmcpeak@payrailz.com>
2022-06-09 12:33:01 -0700

    use our example

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import 'openapi-explorer';
 import { createRoot } from 'react-dom/client';
-import App from './App';
 import './index.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
-root.render(<App tab="home" />);
+root.render(<openapi-explorer
+  spec-url="http://localhost:3000/test.json"
+  nav-bg-color="#732282"
+  text-color="#555555"
+  secondary-color="#f3e5f5" />);
